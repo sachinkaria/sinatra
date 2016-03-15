b70b9fb04cd58533a3a86849524ef93e5580fe3c
Chris Parker <cjp214@gmail.com>
2016-03-15 10:54:12 +0000

    moved @catname out of the index.erb file

--- app.rb
+++ app.rb
@@ -5,18 +5,7 @@ get '/' do
   "hello!"
 end
 
-get '/secret' do
-  "how very secretive"
-end
-
 get '/cat' do
-  erb(:index)
-end
-
-get '/cheese' do
-  "if you don't like cheese you should leave right now"
-end
-
-get '/baseball' do
-  "based ball"
+  @catname = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)  
 end
